892b4120135ef62ca22837bc0e1d79ada87203b6
Josh Dowling <jk.dowling22@gmail.com>
2023-10-05 20:12:49 +1100

    Add functions to gamers

--- canasta/game_lib/src/game.rs
+++ canasta/game_lib/src/game.rs
@@ -1,5 +1,27 @@
 
 pub struct CanastaGame {
     game_id: u32,
-    
+        
+}
+
+impl CanastaGame {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn draw(&mut self) {
+        todo!()
+    }
+
+    pub fn discard(&mut self) {
+        todo!()
+    }
+
+    pub fn meld(&mut self) {
+        todo!()
+    }
+
+    pub fn take_discard(&mut self) {
+        todo!()
+    }
 }
